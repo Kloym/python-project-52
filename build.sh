6b89184324af69ba0790de08b4485325b3d95b1b
Kloym <oleg.petr1223@gmail.com>
2025-05-23 12:53:15 +0300

    add requirements

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+source .venv/bin/activate
+uv pip install -r requirements.txt
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
