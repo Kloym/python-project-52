fcb85d70d3532c4550c9b5cd2c8e229520dac084
Kloym <oleg.petr1223@gmail.com>
2025-05-28 09:30:23 +0300

    f

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# установите переменную окружения
-export DJANGO_SETTINGS_MODULE=task_manager.settings
-
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
 make install && make collectstatic && make migrate
