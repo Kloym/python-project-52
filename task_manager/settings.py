2da1d512bb776ff577fec5ed21f44c0574aa2b58
Kloym <oleg.petr1223@gmail.com>
2025-05-28 10:40:12 +0300

    l

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -148,10 +148,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-DJANGO_SETTINGS_MODULE = 'task_manager.settings'
