b9635da5b0e33cee5f7b56b7526f449453345196
Kloym <oleg.petr1223@gmail.com>
2025-06-02 14:32:33 +0300

    h

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from task_manager.labels import views
 
 urlpatterns = [
-    path("", views.label_list, name="label_list"),
-    path("create/", views.create_label, name="create_label"),
-    path("<int:pk>/update/", views.label_update, name="update_label"),
-    path("<int:pk>/delete/", views.label_delete, name="delete_label"),
+    path("", views.IndexView.as_view(), name="label_list"),
+    path("create/", views.LabelCreateView.as_view(), name="create_label"),
+    path("<int:pk>/update/", views.LabelUpdateView.as_view(), name="update_label"),
+    path("<int:pk>/delete/", views.LabelDeleteView.as_view(), name="delete_label"),
 ]
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,51 +1,57 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import LabelForm
-from .models import Label
-from task_manager.tasks.models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from task_manager.labels.forms import LabelForm
+from task_manager.labels.models import Label
 
+class IndexView(LoginRequiredMixin, ListView):
+    model = Label
+    template_name = 'labels/label_list.html'
+    context_object_name = 'labels'
 
-def label_list(request):
-    labels = Label.objects.all()
-    return render(request, "labels/label_list.html", {"labels": labels})
+class LabelCreateView(LoginRequiredMixin, CreateView):
+    form_class = LabelForm
+    template_name = 'labels/create_label.html'
+    success_url = reverse_lazy('label_list')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = False
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, _("Метка успешно создана"))
+        return super().form_valid(form)
+    
+class LabelUpdateView(LoginRequiredMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/label_update.html'
+    success_url = reverse_lazy('label_list')
 
-def create_label(request):
-    if request.method == "POST":
-        form = LabelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Метка успешно создана")
-            return redirect("label_list")
-        else:
-            print(form.errors)
-    else:
-        form = LabelForm()
-    return render(request, "labels/create_label.html", {"form": form})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = True
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, _("Метка успешно изменена"))
+        return super().form_valid(form)
+    
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
+    model = Label
+    template_name = 'labels/label_confirm_delete.html'
+    success_url = reverse_lazy('label_list')
+    context_object_name = 'label'
 
-
-def label_update(request, pk):
-    label = get_object_or_404(Label, pk=pk)
-    if request.method == "POST":
-        form = LabelForm(request.POST, instance=label)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Метка успешно изменена")
-            return redirect("label_list")
-    else:
-        form = LabelForm(instance=label)
-    return render(request, "labels/label_update.html", {"form": form, "label": label})
-
-
-def label_delete(request, pk):
-    label = get_object_or_404(Label, pk=pk)
-    if request.method == "POST":
-        if Task.objects.filter(labels=label).exists():
-            messages.error(
-                request, "Невозможно удалить метку, потому что она используется"
-            )
-            return redirect("label_list")
-        label.delete()
-        messages.success(request, "Метка успешно удалена")
-        return redirect("label_list")
-    return render(request, "labels/label_confirm_delete.html", {"label": label})
+    def form_valid(self, form):
+        label = self.get_object()
+        if label.task_set.exists():
+            messages.error(self.request, 
+                           _('Невозможно удалить метку, потому что она используется'))
+            return redirect(self.success_url)
+        messages.success(self.request, _('Метка успешно удалена'))
+        return super().form_valid(form)
