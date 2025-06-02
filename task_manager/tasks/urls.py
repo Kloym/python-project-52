fd37ade723a519873ee8a341e3bc6dd2153e331e
Kloym <oleg.petr1223@gmail.com>
2025-06-02 18:02:56 +0300

    k

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("create/", views.TaskCreateView.as_view(), name="create_task"),
     path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="delete_task"),
     path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="update_task"),
-    path("<int:pk>/view/", views.TaskDetailView.as_view(), name="get_task"),
+    path("<int:pk>/", views.TaskDetailView.as_view(), name="get_task"),
 ]
